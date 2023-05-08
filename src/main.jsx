cd926ab826d2f0de1bb44a0a87a7130a9f585b9e
wnukiewiczofficial <wnukiewiczofficial@yahoo.com>
2023-05-08 02:53:32 +0200

    s

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Routing from "./Routing";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/ctx.Playground">
+  <HashRouter>
     <Routing />
-  </BrowserRouter>
+  </HashRouter>
 );
